fc42a3479b199e73bee87bd51c3bc191a2876bc5
Pap Tamas <paptamas@gmail.com>
2013-05-06 19:33:55 +0300

    Implement the Email_Exception class.

---
+++ classes/Email/Exception.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/*
+ * @package		Helpers
+ * @author      Pap Tamas
+ * @copyright   (c) 2011-2012 Pap Tamas
+ * @website		https://github.com/paptamas/kohana-email
+ * @license		http://www.opensource.org/licenses/isc-license.txt
+ *
+ */
+
+class Email_Exception extends Kohana_Email_Exception {
+
+}
+
+// End Email_Exception
